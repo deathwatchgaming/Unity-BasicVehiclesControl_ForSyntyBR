f19ae089f40ac57bfe2046bddabb3da9dda30adb
deathwatchgaming <deathwatchgaming@gmail.com>
2024-05-29 18:36:32 -0700

    Update DuneBuggyController.cs

--- Assets/VehiclesControl/DuneBuggy/Scripts/DuneBuggyController.cs
+++ Assets/VehiclesControl/DuneBuggy/Scripts/DuneBuggyController.cs
@@ -9,11 +9,20 @@
 // Wheels Collider Position Y: (0.4145404+0.15)
 
 // using
+using System;
 using UnityEngine;
 
 // namespace VehiclesControl
 namespace VehiclesControl
 {
+	// public enum DuneBuggySpeedType
+	public enum DuneBuggySpeedType
+	{
+		mph,
+		kmh	
+
+	} // close public enum DuneBuggySpeedType
+		
 	// RequireComponent typeof MeshCollider
 	[RequireComponent(typeof(MeshCollider))]
 
@@ -110,7 +119,18 @@ namespace VehiclesControl
 		    
 			// _currentTurnAngle is 0 
 			private float _currentTurnAngle = 0f;
-	        
+
+		// Speed
+		[Header("Speed")]
+
+			[Tooltip("The speed measurement unit")]
+			// DuneBuggySpeedType _speedType	
+			[SerializeField] private DuneBuggySpeedType _speedType;			
+	    
+			[Tooltip("The maximum speed amount")]
+			// float _maxSpeed
+			[SerializeField] private float _maxSpeed = 180;	
+
 		// private void Awake
 		private void Awake()
 		{
@@ -126,128 +146,168 @@ namespace VehiclesControl
 
 		} // close private void Awake
 
-	    // private void FixedUpdate
-	    private void FixedUpdate()
-	    {
-	    	// Get the forward and reverse _acceleration from vertical axis (W and S keys)
+		// private void Update
+		private void Update()
+		{
+			// float _speed
+			float _speed = _rigidbody.velocity.magnitude;
+
+			// _speedType equals DuneBuggySpeedType.mph
+			if (_speedType == DuneBuggySpeedType.mph)
+			{
+				// _speed
+				_speed *= 2.23694f;
+
+				// if _speed > _maxSpeed
+				if (_speed > _maxSpeed)
+				{
+					// _rigidbody.velocity
+					_rigidbody.velocity = (_maxSpeed/2.23694f) * _rigidbody.velocity.normalized;
+
+				} // close if _speed > _maxSpeed
+                        
+			} // close if _speedType equals DuneBuggySpeedType.mph
+
+			// else if _speedType equals DuneBuggySpeedType.kmh
+			else if (_speedType == DuneBuggySpeedType.kmh)
+			{
+				// _speed
+				_speed *= 3.6f;
+
+				// if _speed > _maxSpeed
+				if (_speed > _maxSpeed)
+				{
+					// _rigidbody.velocity
+					_rigidbody.velocity = (_maxSpeed/3.6f) * _rigidbody.velocity.normalized;
+
+				} // close if _speed > _maxSpeed
+                       
+			} // close else if _speedType equals DuneBuggySpeedType.kmh
+
+		} // close private void Update
+
+		// private void FixedUpdate
+		private void FixedUpdate()
+		{
+			// Get the forward and reverse _acceleration from vertical axis (W and S keys)
 	        
-	        // _currentAcceleration is _acceleration times Input GetAxis Vertical
-	        _currentAcceleration = _acceleration * Input.GetAxis(_verticalMoveInput);
+			// _currentAcceleration is _acceleration times Input GetAxis Vertical
+			_currentAcceleration = _acceleration * Input.GetAxis(_verticalMoveInput);
 
-	        // If we are pressing the space key give currentBrakingForce a value
+			// If we are pressing the space key give currentBrakingForce a value
 
-	    	// if Input GetKey KeyCode Space
-	    	if (Input.GetKey(_brakeKey))
-	    	{
-	    		// _currentBrakeForce is _brakingForce
-	    		_currentBrakeForce = _brakingForce;
+			// if Input GetKey KeyCode Space
+			if (Input.GetKey(_brakeKey))
+			{
+				// _currentBrakeForce is _brakingForce
+				_currentBrakeForce = _brakingForce;
 
-	    	} // close if Input GetKey KeyCode Space
+			} // close if Input GetKey KeyCode Space
 	        
-	        // else 
-	    	else
-	    	{
-	    		// _currentBrakeForce is 0
-	    		_currentBrakeForce = 0f;
+			// else 
+			else
+			{
+				// _currentBrakeForce is 0
+				_currentBrakeForce = 0f;
 
-	    	} // close else
+			} // close else
 
-	    	// Apply _acceleration to the front wheels
+			// Apply _acceleration to the front wheels
 	        
-	        // _frontLeft motorTorque is _currentAcceleration
-	    	_frontLeft.motorTorque = _currentAcceleration;
+			// _frontLeft motorTorque is _currentAcceleration
+			_frontLeft.motorTorque = _currentAcceleration;
 
-	        // _frontRight motorTorque is _currentAcceleration
-	    	_frontRight.motorTorque = _currentAcceleration;
+			// _frontRight motorTorque is _currentAcceleration
+			_frontRight.motorTorque = _currentAcceleration;
 
-	        // Apply braking force to all wheels
+			// Apply braking force to all wheels
 
-	        // _frontLeft brakeTorque is _currentBrakeForce
-	        _frontLeft.brakeTorque = _currentBrakeForce;
+			// _frontLeft brakeTorque is _currentBrakeForce
+			_frontLeft.brakeTorque = _currentBrakeForce;
 
-	        // _frontRight brakeTorque is _currentBrakeForce
-	        _frontRight.brakeTorque = _currentBrakeForce;
+			// _frontRight brakeTorque is _currentBrakeForce
+			_frontRight.brakeTorque = _currentBrakeForce;
 
-	        // _rearLeft brakeTorque is _currentBrakeForce
-	        _rearLeft.brakeTorque = _currentBrakeForce;
+			// _rearLeft brakeTorque is _currentBrakeForce
+			_rearLeft.brakeTorque = _currentBrakeForce;
 
-	        // _rearRight brakeTorque is _currentBrakeForce
-	        _rearRight.brakeTorque = _currentBrakeForce;
+			// _rearRight brakeTorque is _currentBrakeForce
+			_rearRight.brakeTorque = _currentBrakeForce;
 
-	        // Take care of the front wheels steering
+			// Take care of the front wheels steering
 
-	        // _currentTurnAngle is _maxTurnAngle times Input GetAxis Horizontal
-	        _currentTurnAngle = _maxTurnAngle * Input.GetAxis(_horizontalMoveInput);
+			// _currentTurnAngle is _maxTurnAngle times Input GetAxis Horizontal
+			_currentTurnAngle = _maxTurnAngle * Input.GetAxis(_horizontalMoveInput);
 
-	        // _frontLeft steerAngle is _currentTurnAngle
-	        _frontLeft.steerAngle = _currentTurnAngle;
+			// _frontLeft steerAngle is _currentTurnAngle
+			_frontLeft.steerAngle = _currentTurnAngle;
 
-	        // _frontRight steerAngle is _currentTurnAngle
-	        _frontRight.steerAngle = _currentTurnAngle;
+			// _frontRight steerAngle is _currentTurnAngle
+			_frontRight.steerAngle = _currentTurnAngle;
 
-	        // Update wheel meshes
+			// Update wheel meshes
 
-	        // UpdateLeftWheel _frontLeft _frontLeftTransform
-	        UpdateLeftWheel(_frontLeft, _frontLeftTransform); 
+			// UpdateLeftWheel _frontLeft _frontLeftTransform
+			UpdateLeftWheel(_frontLeft, _frontLeftTransform); 
 
-	        // UpdateRightWheel _frontRight _frontRightTransform
-	        UpdateRightWheel(_frontRight, _frontRightTransform);
+			// UpdateRightWheel _frontRight _frontRightTransform
+			UpdateRightWheel(_frontRight, _frontRightTransform);
 
-	        // UpdateLeftWheel _rearLeft _rearLeftTransform
-	        UpdateLeftWheel(_rearLeft, _rearLeftTransform); 
+			// UpdateLeftWheel _rearLeft _rearLeftTransform
+			UpdateLeftWheel(_rearLeft, _rearLeftTransform); 
 	        
-	        // UpdateRightWheel _rearRight _rearRightTransform
-	        UpdateRightWheel(_rearRight, _rearRightTransform);                      
+			// UpdateRightWheel _rearRight _rearRightTransform
+			UpdateRightWheel(_rearRight, _rearRightTransform);                      
 
-	    } // close private void FixedUpdate
+		} // close private void FixedUpdate
 
-	    // private void UpdateLeftWheel WheelCollider _leftCollider Transform _leftTransform
-	    private void UpdateLeftWheel(WheelCollider _leftCollider, Transform _leftTransform)
-	    {
-	    	// Get the left wheels collider states
+		// private void UpdateLeftWheel WheelCollider _leftCollider Transform _leftTransform
+		private void UpdateLeftWheel(WheelCollider _leftCollider, Transform _leftTransform)
+		{
+			// Get the left wheels collider states
 
-	    	// Vector3 _leftPosition
-	    	Vector3 _leftPosition;
+			// Vector3 _leftPosition
+			Vector3 _leftPosition;
 
-	    	// Quaternion _leftRotation
-	    	Quaternion _leftRotation;
+			// Quaternion _leftRotation
+			Quaternion _leftRotation;
 
-	    	// _leftCollider GetWorldPose out _leftPosition out _leftRotation
-	    	_leftCollider.GetWorldPose(out _leftPosition, out _leftRotation);
+			// _leftCollider GetWorldPose out _leftPosition out _leftRotation
+			_leftCollider.GetWorldPose(out _leftPosition, out _leftRotation);
 
-	    	// Set the left wheels transform states
+			// Set the left wheels transform states
 
-	    	// _leftTransform position is _leftPosition
-	    	_leftTransform.position = _leftPosition;
+			// _leftTransform position is _leftPosition
+			_leftTransform.position = _leftPosition;
 
-	    	// _leftTransform rotation is _leftRotation
-	    	_leftTransform.rotation = _leftRotation;
+			// _leftTransform rotation is _leftRotation
+			_leftTransform.rotation = _leftRotation;
 
-	    } // close private void UpdateLeftWheel WheelCollider _leftCollider Transform _leftTransform
+		} // close private void UpdateLeftWheel WheelCollider _leftCollider Transform _leftTransform
 	    
-	    // private void UpdateRightWheel WheelCollider _rightCollider Transform _rightTransform
-	    private void UpdateRightWheel(WheelCollider _rightCollider, Transform _rightTransform)
-	    {
-	    	// Get the right wheels collider states
+		// private void UpdateRightWheel WheelCollider _rightCollider Transform _rightTransform
+		private void UpdateRightWheel(WheelCollider _rightCollider, Transform _rightTransform)
+		{
+			// Get the right wheels collider states
 
-	    	// Vector3 _rightPosition
-	    	Vector3 _rightPosition;
+			// Vector3 _rightPosition
+			Vector3 _rightPosition;
 
-	    	// Quaternion _rightRotation
-	    	Quaternion _rightRotation;
+			// Quaternion _rightRotation
+			Quaternion _rightRotation;
 
-	    	// _rightCollider GetWorldPose out _rightPosition out _rightRotation
-	    	_rightCollider.GetWorldPose(out _rightPosition, out _rightRotation);
+			// _rightCollider GetWorldPose out _rightPosition out _rightRotation
+			_rightCollider.GetWorldPose(out _rightPosition, out _rightRotation);
 
-	    	// Set the right wheels transform states
+			// Set the right wheels transform states
 
-	    	// _rightTransform position is _rightPosition
-	    	_rightTransform.position = _rightPosition;
+			// _rightTransform position is _rightPosition
+			_rightTransform.position = _rightPosition;
 
-	    	// _rightTransform rotation is _rightRotation
-	    	_rightTransform.rotation = _rightRotation;    	
+			// _rightTransform rotation is _rightRotation
+			_rightTransform.rotation = _rightRotation;    	
 
-	    } // close private void UpdateRightWheel WheelCollider _rightCollider Transform _rightTransform
+		} // close private void UpdateRightWheel WheelCollider _rightCollider Transform _rightTransform
 		
 	} // close public class DuneBuggyController
 
